51c566836c2f26e8b4ee53d46cb74035eadb5dbe
wakfu <wakfu@DESKTOP-0D21SDO>
2024-06-07 23:19:22 +0300

    Добавьте файлы проекта.

---
+++ Лаба4/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба4
+{
+    public abstract class Person
+    {
+        public string? FIO { get; set; }
+    }
+
+    public class Employee : Person
+    {
+        public int CertificateNumber { get; set; }
+    }
+
+    public class Student : Person
+    {
+        public int RecordBookNumber { get; set; }
+    }
+
+    public class StaffList
+    {
+        public List<Person> Persons { get; set; }
+
+        public StaffList()
+        {
+            Persons = new List<Person>();
+        }
+
+        public void AddPerson(Person person)
+        {
+            Persons.Add(person);
+        }
+
+        public int GetEmployeeCount()
+        {
+            return Persons.Count(p => p is Employee);
+        }
+
+        public int GetStudentCount()
+        {
+            return Persons.Count(p => p is Student);
+        }
+    }
+}
---
+++ Лаба4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Лаба4{
+    class Program{
+        static void Main(){
+
+    
+
+StaffList staffList = new StaffList();
+
+staffList.AddPerson(new Employee { FIO = "Иванов Иван Иванович", CertificateNumber = 12345 });
+staffList.AddPerson(new Student { FIO = "Петров Петр Петрович", RecordBookNumber = 11111 });
+staffList.AddPerson(new Employee { FIO = "Сидоров Сидор Сидорович", CertificateNumber = 67890 });
+staffList.AddPerson(new Student { FIO = "Кузнецов Кузьма Кузьмич", RecordBookNumber = 22222 });
+staffList.AddPerson(new Employee { FIO = "Лебедев Лев Лебедевич", CertificateNumber = 34567 });
+
+var employeeCount = staffList.GetEmployeeCount();
+var studentCount = staffList.GetStudentCount();
+
+Console.WriteLine("Список персонала:");
+foreach (var person in staffList.Persons)
+{
+    if (person is Employee)
+    {
+        Console.WriteLine($"Преподаватель: {person.FIO}, № удостоверения: {((Employee)person).CertificateNumber}");
+    }
+    else if (person is Student)
+    {
+        Console.WriteLine(value: $"Студент: {person.FIO}, № зачётки: {((Student)person).RecordBookNumber}");
+    }
+}
+Console.WriteLine($"Количество преподавателей: {employeeCount}");
+Console.WriteLine($"Количество студентов: {studentCount}");
+ }
+    }
+}
